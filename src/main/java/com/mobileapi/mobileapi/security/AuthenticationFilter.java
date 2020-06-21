715b5fc86b42495995d0eb53870f5ca4c04bc254
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2020-06-21 14:11:40 -0300

    feat: Filter to check login credentials

---
+++ src/main/java/com/mobileapi/mobileapi/security/AuthenticationFilter.java
@@ -0,0 +1,68 @@
+package com.mobileapi.mobileapi.security;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mobileapi.mobileapi.ui.model.request.UserLoginRequestModel;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)          throws AuthenticationException {
+        try {
+            /**
+             * Gets data from request object into UserLoginRequestModel instance
+             */
+            UserLoginRequestModel credentials = new ObjectMapper()
+                .readValue(request.getInputStream(), UserLoginRequestModel.class);
+
+            return authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                    credentials.getEmail()
+                , credentials.getPassword()
+                )
+            );
+            
+        } catch (Exception e) {
+            throw new RuntimeException(e);        
+        }        
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        String userName = ((User) authResult.getPrincipal()).getUsername();
+
+        // Builds jwt
+        String token = Jwts.builder()
+        .setSubject(userName)
+        .setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME)).
+        // signature method
+        signWith(SignatureAlgorithm.HS512, SecurityConstants.TOKEN_SECRET)
+        .compact();
+        
+        // add to the response header
+        response.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
+    }
+
+}
