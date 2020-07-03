9bb72161211973de44c2ea30b0f8e56119634bd9
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2020-07-03 19:59:42 -0300

    chore: basic controller update structure

--- src/main/java/com/mobileapi/mobileapi/ui/UserController.java
+++ src/main/java/com/mobileapi/mobileapi/ui/UserController.java
@@ -11,6 +11,7 @@ import com.mobileapi.mobileapi.ui.model.response.UserRest;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,45 +25,55 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("users")
 public class UserController {
-    @Autowired
-    UserService userService;
+	@Autowired
+	UserService userService;
 
-    @GetMapping(path = "/{id}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
-    public UserRest getUser(@PathVariable String id) {
-        UserRest returnValue = new UserRest();
+	@GetMapping(path = "/{id}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+	public UserRest getUser(@PathVariable String id) {
+		UserRest returnValue = new UserRest();
 
-        UserDto userFound = userService.getUserByUserId(id);
-        if (userFound == null)
-            throw new UsernameNotFoundException(id);
+		UserDto userFound = userService.getUserByUserId(id);
+		if (userFound == null)
+			throw new UsernameNotFoundException(id);
 
-        BeanUtils.copyProperties(userFound, returnValue);
+		BeanUtils.copyProperties(userFound, returnValue);
 
-        return returnValue;
-    }
+		return returnValue;
+	}
 
-    @PostMapping(produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, consumes = {
-            MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
-    public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) throws Exception {
-        UserRest returnValue = new UserRest();
+	@PostMapping(produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, consumes = {
+			MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) throws Exception {
+		UserRest returnValue = new UserRest();
 
-        if (userDetails.getFirstName().isEmpty())
-            throw new UserServiceException(ErrorMessages.MISSING_REQUIRED_FIELD.getErrorMessage());
-        UserDto userDto = new UserDto();
-        BeanUtils.copyProperties(userDetails, userDto);
+		if (userDetails.getFirstName().isEmpty())
+			throw new UserServiceException(ErrorMessages.MISSING_REQUIRED_FIELD.getErrorMessage());
+		UserDto userDto = new UserDto();
+		BeanUtils.copyProperties(userDetails, userDto);
 
-        UserDto createdUser = userService.createUser(userDto);
-        BeanUtils.copyProperties(createdUser, returnValue);
-        return returnValue;
-    }
+		UserDto createdUser = userService.createUser(userDto);
+		BeanUtils.copyProperties(createdUser, returnValue);
+		return returnValue;
+	}
 
-    @PutMapping
-    public String putMethodName() {
-        return "updating";
-    }
+	@PutMapping(path = "/{id}", produces = { MediaType.APPLICATION_XML_VALUE,
+			MediaType.APPLICATION_JSON_VALUE }, consumes = { MediaType.APPLICATION_XML_VALUE,
+					MediaType.APPLICATION_JSON_VALUE })
+	public UserRest updateUser(@PathVariable String id, @RequestBody UserDetails userDetails) {
+		UserRest returnValue = new UserRest();
 
-    @DeleteMapping
-    public String deleteUser() {
-        return "deleteing";
-    }
+		UserDto userDto = new UserDto();
+		BeanUtils.copyProperties(userDetails, userDto);
+
+		UserDto updatedUser = userService.updateUser(userDto);
+		BeanUtils.copyProperties(updatedUser, returnValue);
+
+		return returnValue;
+	}
+
+	@DeleteMapping
+	public String deleteUser() {
+		return "deleteing";
+	}
 
 }
