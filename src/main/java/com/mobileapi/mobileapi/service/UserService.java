56f476b0161f02c13563a55d446d6a3ac2e4189d
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2020-07-03 20:31:56 -0300

    chore: added deleteUser signature

--- src/main/java/com/mobileapi/mobileapi/service/UserService.java
+++ src/main/java/com/mobileapi/mobileapi/service/UserService.java
@@ -12,4 +12,6 @@ public interface UserService extends UserDetailsService {
     UserDto getUserByUserId(String userId);
 
     UserDto updateUser(String id, UserDto userDto);
+
+    void deleteUser(String id);
 }
