f8d56b37d0af1b0f3bbb88f4264f00a2a522ecad
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2020-07-03 21:09:33 -0300

    feat: added pagination interface

--- src/main/java/com/mobileapi/mobileapi/ui/io/repositories/UserRepository.java
+++ src/main/java/com/mobileapi/mobileapi/ui/io/repositories/UserRepository.java
@@ -2,11 +2,11 @@ package com.mobileapi.mobileapi.ui.io.repositories;
 
 import com.mobileapi.mobileapi.ui.io.entity.UserEntity;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
+public interface UserRepository extends PagingAndSortingRepository<UserEntity, Long> {
     UserEntity findByEmail(String email);
 
     UserEntity findByUserId(String email);
