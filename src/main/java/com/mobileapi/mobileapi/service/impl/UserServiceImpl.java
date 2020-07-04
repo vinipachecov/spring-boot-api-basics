c85b0b8f800e92e55d43263f0ce59224ae0d14e6
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2020-07-03 21:09:13 -0300

    feat: get Users service implementation

--- src/main/java/com/mobileapi/mobileapi/service/impl/UserServiceImpl.java
+++ src/main/java/com/mobileapi/mobileapi/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.mobileapi.mobileapi.service.impl;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.mobileapi.mobileapi.exceptions.UserServiceException;
 import com.mobileapi.mobileapi.service.UserService;
@@ -12,6 +13,9 @@ import com.mobileapi.mobileapi.ui.model.response.ErrorMessages;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -106,6 +110,27 @@ public class UserServiceImpl implements UserService {
             throw new UserServiceException(ErrorMessages.NO_RECORD_FOUND.getErrorMessage());
 
         userRepository.delete(userEntity);
+    }
+
+    @Override
+    public List<UserDto> getUsers(int page, int limit) {
+        List<UserDto> returnValue = new ArrayList<>();
+
+        if (page > 1)
+            page -= 1;
+        // create a page
+        Pageable pageableRequest = PageRequest.of(page, limit);
+
+        // get our page of users
+        Page<UserEntity> usersPage = userRepository.findAll(pageableRequest);
+        List<UserEntity> users = usersPage.getContent();
+        for (UserEntity userEntity : users) {
+            UserDto userDto = new UserDto();
+            BeanUtils.copyProperties(userEntity, userDto);
+            returnValue.add(userDto);
+        }
+
+        return returnValue;
     }
 
 }
