4460d25e4aee61b3dc26a0e9cd86dc79ae89b08c
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2020-07-04 20:18:00 -0300

    feat: open email-verification url

--- src/main/java/com/mobileapi/mobileapi/security/WebSecurity.java
+++ src/main/java/com/mobileapi/mobileapi/security/WebSecurity.java
@@ -19,30 +19,26 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     public WebSecurity(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userService = userService;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-    }   
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
-        .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
-        .permitAll()
-        .anyRequest().authenticated()
-        .and()
-        .addFilter(getAuthenticationFilter())
-        .addFilter(new AuthorizationFilter(authenticationManager()))
-        .sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);        
-    }    
+        http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
+                .permitAll().antMatchers(HttpMethod.GET, SecurityConstants.VERIFICATION_URL).permitAll().anyRequest()
+                .authenticated().and().addFilter(getAuthenticationFilter())
+                .addFilter(new AuthorizationFilter(authenticationManager())).sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
     }
 
-     public AuthenticationFilter getAuthenticationFilter() throws Exception {
+    public AuthenticationFilter getAuthenticationFilter() throws Exception {
         final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
         filter.setFilterProcessesUrl("/users/login");
         return filter;
     }
-     
+
 }
