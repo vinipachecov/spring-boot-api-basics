2bd3e9c5be90c964b2de045a6f8bef39dd78ebd8
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2020-07-03 21:08:14 -0300

    chore: added getUsers signature

--- src/main/java/com/mobileapi/mobileapi/service/UserService.java
+++ src/main/java/com/mobileapi/mobileapi/service/UserService.java
@@ -1,5 +1,7 @@
 package com.mobileapi.mobileapi.service;
 
+import java.util.List;
+
 import com.mobileapi.mobileapi.shared.dto.UserDto;
 
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -8,6 +10,8 @@ public interface UserService extends UserDetailsService {
     UserDto createUser(UserDto ser);
 
     UserDto getUser(String email);
+
+    List<UserDto> getUsers(int page, int limit);
 
     UserDto getUserByUserId(String userId);
 
