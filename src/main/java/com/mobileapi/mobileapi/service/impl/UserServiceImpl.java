3ecabea2d2c30a1ea90bce25d40a786cfb735a61
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2020-07-04 20:18:36 -0300

    feat: added verifyEmailToken implementation

--- src/main/java/com/mobileapi/mobileapi/service/impl/UserServiceImpl.java
+++ src/main/java/com/mobileapi/mobileapi/service/impl/UserServiceImpl.java
@@ -146,4 +146,23 @@ public class UserServiceImpl implements UserService {
         return returnValue;
     }
 
+    @Override
+    public boolean verifyEmailToken(String token) {
+        boolean returnValue = false;
+
+        UserEntity userEntity = userRepository.findUserByEmailVerificationToken(token);
+
+        if (userEntity != null) {
+            boolean hasTokenExpired = Utils.hasTokenExpired(token);
+            if (!hasTokenExpired) {
+                userEntity.setEmailVerificationToken(null);
+                userEntity.setEmailVerificationStatus(Boolean.TRUE);
+                userRepository.save(userEntity);
+                returnValue = true;
+                return returnValue;
+            }
+        }
+        return returnValue;
+    }
+
 }
