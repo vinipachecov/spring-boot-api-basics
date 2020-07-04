c6da8d49d5ced1bc4593173886ec0f126b4e5e5c
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2020-07-04 20:18:54 -0300

    feat: added utils method to check email verify token validation

--- src/main/java/com/mobileapi/mobileapi/shared/Utils.java
+++ src/main/java/com/mobileapi/mobileapi/shared/Utils.java
@@ -1,9 +1,15 @@
 package com.mobileapi.mobileapi.shared;
 
 import java.security.SecureRandom;
+import java.util.Date;
 import java.util.Random;
 
+import com.mobileapi.mobileapi.security.SecurityConstants;
+
 import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 
 @Component
 public class Utils {
@@ -28,6 +34,15 @@ public class Utils {
 
     public String generateAddressId(int length) {
         return generateRandomString(length);
+    }
+
+    public static boolean hasTokenExpired(String token) {
+        Claims claims = Jwts.parser().setSigningKey(SecurityConstants.getTokenSecret()).parseClaimsJws(token).getBody();
+
+        Date tokenExpireDate = claims.getExpiration();
+        Date todayDate = new Date();
+
+        return tokenExpireDate.before(todayDate);
     }
 
 }
